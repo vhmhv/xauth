b37234d72e843acc0f7e9f7d30ea770808609427
Leo Friedrichs <l.friedrichs@vhmhv.de>
2024-01-04 09:52:19 +0100

    Always load migrations
    This allows for migrations to run when called from a controller (e.g. when request by cypress during tests)

--- src/XAuthServiceProvider.php
+++ src/XAuthServiceProvider.php
@@ -30,9 +30,8 @@ class XAuthServiceProvider extends ServiceProvider
                 ],
                 'views'
             );
-
-            $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
         }
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'xauth');
     }
