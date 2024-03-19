d7d0bd9c4db07881f2aa4f08a6152fefc9fb4b73
Benedikt Hübschen <b.huebschen@vhmhv.de>
2024-03-19 13:36:56 +0100

    tenant

--- config/xauth.php
+++ config/xauth.php
@@ -5,6 +5,8 @@ return [
         'secret' => env('GRAPH_SECRET', 'INVALID SECRET'),
         'key' => env('GRAPH_KEY', 'INVALID KEY'),
         'callback_url' => env('GRAPH_CALLBACK', env('APP_URL') . '/login/graph/callback'),
+        'tenant_id' => env('GRAPH_TENANT_ID', 'common'),
+        'include_tenant_info' => env('GRAPH_INCLUDE_TENANT_INFO', false),
     ],
     'uri' => [
         'login-success' => env('XAUTH_LOGIN_SUCCESS', '/'),
